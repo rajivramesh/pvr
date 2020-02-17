1fe7a4eb9c107800a90de53db1367b868679fb57
Rajivc <rajivchaurisia341@gmail.com>
2020-02-17 19:47:16 +0530

    bug fix

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,10 +1,9 @@
 class NotifyMailer < ActionMailer::Base
 
-	def send_remainder(user_id, show_id)
-		debugger
-		@user = User.find_by_id(user_id)
-		@show = Show.find_by_id(show_id)
-		mail(to: @user.email, subject: "Notification for Show Remainder")
-	end
+  def send_remainder(user_id, show_id)
+    @user = User.find_by_id(user_id)
+    @show = Show.find_by_id(show_id)
+    mail(to: @user.email, subject: "Notification for Show Remainder")
+  end
 
 end
